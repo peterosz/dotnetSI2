04438552c2f1a9a01b38e3efe53c65d28d79cf1a
peter.osz <itharius@gmail.com>
2017-11-06 11:31:15 +0100

    Exercise 4 done

---
+++ LookupCollections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace LookupCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Make the dictionary case insensitive 
+            ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+
+            // Add some items
+            list["Estados Unidos"] = "United States";
+            list["Canadá"] = "Canada";
+            list["España"] = "Spain";
+
+            // Show the results 
+            Console.WriteLine(list["españa"]);
+            Console.WriteLine(list["CANADÁ"]);
+            Console.Read();
+        }
+    }
+}
