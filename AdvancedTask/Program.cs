c27a2415535d594a9b610fc823168641b39d8e80
peter.osz <itharius@gmail.com>
2017-11-08 17:01:02 +0100

    found a solution for the archive to work

--- AdvancedTask/Program.cs
+++ AdvancedTask/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.IO.Compression;
 
 namespace AdvancedTask
 {
@@ -11,6 +12,7 @@ namespace AdvancedTask
     {
         static List<FileInfo> FoundFiles;
         static List<FileSystemWatcher> watchers;
+        static List<DirectoryInfo> archiveDirs;
 
         static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
         {
@@ -29,17 +31,35 @@ namespace AdvancedTask
         static void WatcherChanged(object sender, FileSystemEventArgs e)
         {
             if (e.ChangeType == WatcherChangeTypes.Changed)
-                Console.WriteLine("{0} has been changed!", e.FullPath);
-
-            foreach (FileInfo fil in FoundFiles)
             {
-                FileSystemWatcher newWatcher = new FileSystemWatcher(fil.DirectoryName, fil.Name);
-                newWatcher.Changed += new FileSystemEventHandler(WatcherChanged);
-                newWatcher.EnableRaisingEvents = true;
-                watchers.Add(newWatcher);
+                Console.WriteLine("{0} has been changed!", e.FullPath);
+                //find the the index of the changed file 
+                FileSystemWatcher senderWatcher = (FileSystemWatcher)sender;
+                int index = watchers.IndexOf(senderWatcher, 0);
+                //now that we have the index, we can archive the file
+                System.Threading.Thread.Sleep(1000);
+                ArchiveFile(archiveDirs[index], FoundFiles[index]);
             }
         }
 
+        static void ArchiveFile(DirectoryInfo archiveDir, FileInfo fileToArchive)
+        {
+            FileStream input = fileToArchive.OpenRead();
+            FileStream output = File.Create(archiveDir.FullName + @"\" + fileToArchive.Name + ".gz");
+            GZipStream Compressor = new GZipStream(output, CompressionMode.Compress);
+            int b = input.ReadByte();
+            while (b != -1)
+            {
+                Compressor.WriteByte((byte)b);
+
+                b = input.ReadByte();
+            }
+            Console.WriteLine("archive done");
+            Compressor.Close();
+            input.Close();
+            output.Close();
+        }
+        
         static void Main(string[] args)
         {
             string fileName = args[0];
@@ -63,6 +83,22 @@ namespace AdvancedTask
             {
                 Console.WriteLine("{0}", fil.FullName);
             }
+
+            foreach (FileInfo fil in FoundFiles)
+            {
+                FileSystemWatcher newWatcher = new FileSystemWatcher(fil.DirectoryName, fil.Name);
+                newWatcher.Changed += new FileSystemEventHandler(WatcherChanged);
+                newWatcher.EnableRaisingEvents = true;
+                watchers.Add(newWatcher);
+            }
+
+            archiveDirs = new List<DirectoryInfo>();
+            //create archive directories
+            for (int i = 0; i < FoundFiles.Count; i++)
+            {
+                archiveDirs.Add(Directory.CreateDirectory("archive" + i.ToString()));
+            }
+
             Console.ReadKey();
         }
     }
